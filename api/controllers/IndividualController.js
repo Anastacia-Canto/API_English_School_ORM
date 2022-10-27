c5007e39c6b9a2f26ede9e97d3cb46011f877eae
Anastacia <anaanestesia@gmail.com>
2022-10-27 02:52:26 +0100

    finalizing CRUD

--- api/controllers/IndividualController.js
+++ api/controllers/IndividualController.js
@@ -7,10 +7,61 @@ class IndividualController {
 			//findAll() is a sequelize method that converts to the query SELECT * ...
 			const individuals = await database.Individuals.findAll();
 			return res.status(200).json(individuals);
-		} catch (error) {
-			return res.status(500).json(error.message);
+		} catch (err) {
+			return res.status(500).json(err.message);
 		};
 	};
+
+	static async listById(req, res) {
+		const { id } = req.params;
+
+		try {
+			const individual = await database.Individuals.findOne({ 
+				where: { 
+					id: Number(id) 
+				}
+			});
+			if (!individual) return res.status(400).send({message: 'Empty position.'});
+			else return res.status(200).json(individual);
+		} catch (err){
+			return res.status(404).json(err.message);
+		};
+	};
+
+	static async createIndividual(req, res){
+		const newIndividual = req.body;
+		try {
+			const newCreatedIndividual = await database.Individuals.create(newIndividual);
+			return res.status(201).json(newCreatedIndividual);
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+	static async updateIndividuals(req, res){
+		const { id } = req.params;
+		const newInfo = req.body;
+		try {
+			await database.Individuals.update(newInfo, { where: { id: Number(id) } });
+			const updatedIndividual = await database.Individuals.findOne({ where: { id: Number(id) } });
+			if (!updatedIndividual) return res.status(400).send({message: 'Empty position.'});
+			else return res.status(201).json(updatedIndividual);
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+	static async deleteIndividual(req, res){
+		const { id } = req.params;
+		try {
+			await database.Individuals.destroy({ where: { id: Number(id) } });
+			return res.status(201).send({message: `Id ${id} deleted.`});
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	}
 };
+
+
 
 module.exports = IndividualController;
--- api/routes/individualsRoute.js
+++ api/routes/individualsRoute.js
@@ -3,6 +3,11 @@ const IndividualController = require('../controllers/IndividualController');
 
 const router = Router();
 
-router.get('/individuals', IndividualController.listIndividuals);
+router
+	.get('/individuals', IndividualController.listIndividuals)
+	.get('/individuals/:id', IndividualController.listById)
+	.post('/individuals', IndividualController.createIndividual)
+	.put('/individuals/:id', IndividualController.updateIndividuals)
+	.delete('/individuals/:id', IndividualController.deleteIndividual);
 
 module.exports = router;
