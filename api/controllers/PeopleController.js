4fc795610a50ed807832c60fd9db225817cf0a7f
Anastacia <anaanestesia@gmail.com>
2022-10-27 11:25:05 +0100

    finalizing registrations CRUD

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -59,7 +59,99 @@ class PeopleController {
 		} catch (err){
 			return res.status(500).json(err.message);
 		};
-	}
+	};
+
+	// CRUD for Registrations
+
+	static async listByRegistration(req, res){
+		const { studentId, registrationId } = req.params;
+		try {
+			const registration = await database.Registrations.findOne({ 
+				where: { 
+					id: Number(registrationId),
+					student_id: Number(studentId)
+				}
+			});
+			if (!registration) return res.status(404).send({message: 'Empty registration.'});
+			else return res.status(200).json(registration);
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+	static async createRegistration(req, res){
+		const { studentId } = req.params;
+		const newRegistration = { ...req.body, student_id: Number(studentId)};
+		try {
+			const newCreatedRegistration = await database.Registrations.create(newRegistration);
+			return res.status(201).json(newCreatedRegistration);
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+
+	static async listRegistrationsByStudent(req, res){
+		const { studentId } = req.params;
+		try{
+			const registrations = await database.Registrations.findAll({
+				where: {
+					student_id: Number(studentId)
+				}
+			});
+			if (Object.keys(registrations).length == 0) return res.status(404).send({message: 'There are no registrations to this student.'});
+			else return res.status(200).json(registrations);
+			
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+	static async updateRegistration(req, res){
+		const { studentId, registrationId } = req.params;
+		const newInfo = req.body;
+		try {
+			const actualRegistration = await database.Registrations.findOne({ where: { id: Number(registrationId) } });
+			if (actualRegistration){
+				await database.Registrations.update(newInfo, { 
+					where: { 
+						id: Number(registrationId),
+						student_id: Number(studentId) 
+					} 
+				});
+				const updatedRegistration = await database.Registrations.findOne({ 
+					where: { 
+						id: Number(registrationId) 
+					} 
+				});
+				return res.status(201).json(updatedRegistration);
+			} else {
+				return res.status(400).send({message: 'Empty position.'});
+			};
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
+
+	static async deleteRegistration(req, res){
+		const { studentId, registrationId } = req.params;
+		try {
+			const actualRegistration = await database.Registrations.findOne({ where: { id: Number(registrationId) } });
+			if (actualRegistration){
+				await database.Registrations.destroy({ 
+					where: { 
+						id: Number(registrationId),
+						student_id: Number(studentId)
+					} 
+				});
+				return res.status(201).send({message: `Id ${registrationId} deleted.`});
+			} else {
+				return res.status(400).send({message: 'Empty position.'});
+			}
+		} catch (err){
+			return res.status(500).json(err.message);
+		};
+	};
 };
 
 
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -5,9 +5,14 @@ const router = Router();
 
 router
 	.get('/people', PeopleController.listPeople)
+	.get('/people/:studentId/registrations/:registrationId', PeopleController.listByRegistration)
+	.get('/people/:studentId/registrations', PeopleController.listRegistrationsByStudent)
 	.get('/people/:id', PeopleController.listById)
+	.post('/people/:studentId/registrations', PeopleController.createRegistration)
 	.post('/people', PeopleController.createPerson)
 	.put('/people/:id', PeopleController.updatePerson)
-	.delete('/people/:id', PeopleController.deletePerson);
+	.put('/people/:studentId/registrations/:registrationId', PeopleController.updateRegistration)
+	.delete('/people/:id', PeopleController.deletePerson)
+	.delete('/people/:studentId/registrations/:registrationId', PeopleController.deleteRegistration);
 
 module.exports = router;
